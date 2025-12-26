3f5137e9acdebdff42d6174ce361942129802fbe
nadobrranichh <nazarshch26@gmail.com>
2025-12-26 23:10:10 +0200

    feat: updated header to not render categories on the cart page

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,12 +1,13 @@
 import { Suspense } from "react";
-import { Await, useRouteLoaderData } from "react-router-dom";
+import { Await, Link, useLocation, useRouteLoaderData } from "react-router-dom";
 import ErrorBlock from "./ErrorBlock";
 import classes from "./Header.module.css";
-import HamburgerMenuImg from "../../assets/hamburger-svgrepo-com.svg";
+import CartImg from "../../assets/cart-shopping-svgrepo-com.svg";
 import { useAppSelector, useAppDispatch } from "../../store/custom-hooks";
 import { selectedCategoriesActions } from "../../store/selected-categories-slice";
 
 export default function Header() {
+  const location = useLocation();
   const dispatch = useAppDispatch();
   const { categories } = useRouteLoaderData("root");
   const selectedCategories = useAppSelector(
@@ -23,33 +24,37 @@ export default function Header() {
     <header>
       <div className={classes.container}>
         <h1>DummyShop</h1>
-        <img src={HamburgerMenuImg} className={classes["hamburger-menu"]} />
+        <Link to="/cart">
+          <img src={CartImg} className={classes["hamburger-menu"]} />
+        </Link>
       </div>
-      <div className={classes["categories-container"]}>
-        <Suspense
-          fallback={<p className="loading-text">Loading categories...</p>}
-        >
-          <Await resolve={categories} errorElement={<ErrorBlock />}>
-            {(resolvedCategories) => (
-              <ul>
-                {resolvedCategories.map((category: string) => (
-                  <li
-                    className={`${classes.category} ${
-                      selectedCategories.includes(category)
-                        ? classes.active
-                        : ""
-                    }`}
-                    key={category}
-                    onClick={() => toggleCategorySelection(category)}
-                  >
-                    {category}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </Await>
-        </Suspense>
-      </div>
+      {location.pathname === "/cart" || (
+        <div className={classes["categories-container"]}>
+          <Suspense
+            fallback={<p className="loading-text">Loading categories...</p>}
+          >
+            <Await resolve={categories} errorElement={<ErrorBlock />}>
+              {(resolvedCategories) => (
+                <ul className={classes["categories-list"]}>
+                  {resolvedCategories.map((category: string) => (
+                    <li
+                      className={`${classes.category} ${
+                        selectedCategories.includes(category)
+                          ? classes.active
+                          : ""
+                      }`}
+                      key={category}
+                      onClick={() => toggleCategorySelection(category)}
+                    >
+                      {category}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </Await>
+          </Suspense>
+        </div>
+      )}
     </header>
   );
 }
