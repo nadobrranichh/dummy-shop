05e0aee2f51370ac3cdabe449f39fcf67d48a54a
nadobrranichh <nazarshch26@gmail.com>
2025-12-29 02:04:19 +0200

    feat: made home page adaptive

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -4,25 +4,18 @@ import { priceFormatter } from "../utils/formatters.ts";
 import RatingContainer from "./RatingContainer.tsx";
 import { Link } from "react-router-dom";
 
-const MAX_TITLE_LENGTH = 23;
-
 export default function ProductItem({
   product,
   ...props
 }: {
   product: Product;
 }) {
-  const displayTitle =
-    product.title.length > MAX_TITLE_LENGTH
-      ? product.title.slice(0, MAX_TITLE_LENGTH - 3).trim() + "..."
-      : product.title;
-
   return (
     <li {...props}>
       <Link to={`product?id=${product.id}`} className={classes["product-item"]}>
         <img className={classes["product-image"]} src={product.image} />
         <div className={classes["w-100"]}>
-          <p>{displayTitle}</p>
+          <p>{product.title.trim()}</p>
           <p className="price-text">{priceFormatter.format(product.price)}</p>
         </div>
         <RatingContainer ratingObj={product.rating} />
