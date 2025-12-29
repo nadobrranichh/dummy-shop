c0eaf69dd33e16dfc637330568a6dac6cc5eb94d
nadobrranichh <nazarshch26@gmail.com>
2025-12-29 17:46:22 +0200

    fix: took out unneeded page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
 import HomePage from "./pages/HomePage";
-import CategoryPage from "./pages/CategoryPage";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./http/http";
 import ProductPage from "./pages/ProductPage";
@@ -15,7 +14,6 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: "category", element: <CategoryPage /> },
       { path: "product", element: <ProductPage /> },
       { path: "cart", element: <CartPage /> },
     ],
--- src/pages/CategoryPage.tsx
+++
@@ -1,7 +0,0 @@
-import { useSearchParams } from "react-router-dom";
-
-export default function CategoryPage() {
-  const [searchParams] = useSearchParams();
-  const categoryName = searchParams.get("name");
-  return <div>CategoryPage. name: {categoryName}</div>;
-}
